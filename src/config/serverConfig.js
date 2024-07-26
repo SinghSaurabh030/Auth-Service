2022f7c5b39a7d8f3b90efa7434a7da047317572
Saurabh Singh <singh.saurabh2250@gmail.com>
2024-07-26 19:47:07 +0530

    added n:m association using through table form autorisation

--- src/config/serverConfig.js
+++ src/config/serverConfig.js
@@ -5,5 +5,5 @@ const bcrypt = require('bcrypt');
 module.exports = {
     PORT: process.env.PORT,
     SALT: bcrypt.genSaltSync(10),
-    AUTH_KEY: process.env.AUTH_KEY
+    AUTH_KEY: process.env.AUTH_KEY,
 }
--- src/index.js
+++ src/index.js
@@ -1,32 +1,28 @@
 const express=require('express');
 const app=express();
 const bodyParser=require('body-parser');
-const {PORT}=require('./config/serverConfig');
+const {PORT,DB_SYNC}=require('./config/serverConfig');
 const ApiRoutes=require('./routes/index');
 const UserService = require('./services/user-services');
 const userService=new UserService();
+const db=require('./models/index');
 
 
 
-const setupAndStartServer=()=>{
+const setupAndStartServer=async ()=>{
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended:true}));
     app.use('/api',ApiRoutes);
-    app.listen(PORT,()=>{
+    app.listen(PORT,async ()=>{
         console.log('connected');
-        // const token=userService.createToken({
-        //     email:'saurabh@gmail.com',
-        //     password: 1234
-
-        // })
-        // console.log(token);
-        // console.log(userService.verifyToken('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE3MjE5Njg5MjgsImV4cCI6MTcyMjA1NTMyOH0.WxX_0-Y2hYDrLJmqsN85mRI1_qy7UG0YtIXrN49QV0o'));
-        //  token=userService.signIn({
-        //     email: 'teamupgh@nitjsr.ac.in',
-        //     password: 2222
-        // });
-         console.log(userService,userService.verifyToken('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InRlYW11cGdoQG5pdGpzci5hYy5pbiIsImlhdCI6MTcyMTk3MTIxMiwiZXhwIjoxNzIyMDU3NjEyfQ.E3QgpdIOT4Mv5Eve8JJFFDyUNMYt4YLpiwBVPst98wc'));
-
+        // if(DB_SYNC){
+        //     db.sequelize.sync({alter : true});
+        // }
+        const u1=await db.user.findByPk(10);
+        const r1=await db.role.findByPk(3);
+       // u1.addRole(r1);
+       console.log( await u1.hasRole(r1));
+        
     })
 }
 setupAndStartServer();
